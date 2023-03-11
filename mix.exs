1f1e9e17681478da2215c1012cb62a3e9e916dc4
Greg Hughes <greg.hughes@polkadotskysoftware.com>
2023-03-11 17:37:38 -0500

    changed registration form styling installed timex

--- mix.exs
+++ mix.exs
@@ -50,8 +50,8 @@ defmodule Mctj.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
-
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:timex, "~> 3.0"}
     ]
   end
 
